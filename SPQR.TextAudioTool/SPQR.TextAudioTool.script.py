5fcd0858f155a8d0d92d8f6507d2102f5bfe56f6
SPQR <122448634+5PQR@users.noreply.github.com>
2024-02-21 09:21:08 +0200

    added elevenlabs api calls

--- SPQR.TextAudioTool/SPQR.TextAudioTool.script.py
+++ SPQR.TextAudioTool/SPQR.TextAudioTool.script.py
@@ -1,9 +1,11 @@
 import config
+import os
 import http.server
 import socketserver
 import json
 import importlib
 import subprocess
+from urllib.parse import unquote
 
 
 print("------------------------------------------------------------------------------")
@@ -30,11 +32,15 @@ print("                                                                         
 
 print("------------------------------------------------------------------------------")
 print("    AVAILABLE ENDPOINTS:")
-print("    127.0.0.1:7069/speak POST {text,voice,file} - reads text to audio file")
+print("    127.0.0.1:7069/speak POST - reads text to audio file")
 print("    127.0.0.1:7069/read GET                     - reads files/tts_read.wav ") 
 print("    127.0.0.1:7069/status GET                   - returns the status of the tool")
 print("    127.0.0.1:7069/voices GET                   - returns list of available voices")
 print("    127.0.0.1:7069/listen-whisper GET           - listens and returns speech to text")
+print("    127.0.0.1:7069/speakelevenlabs POST - reads text to audio file with elevenlabs")
+print("    127.0.0.1:7069/userelevenlabs GET - gets elevenlabs user available tokens count")
+print("    127.0.0.1:7069/modelselevenlabs GET - gets elevenlabs available AI models")
+print("    127.0.0.1:7069/voiceselevenlabs GET - gets elevenlabs available voices")
 print("    More details and endpoint tests in demo/demo.html")
 print("------------------------------------------------------------------------------")
 print("    Help me make more cool stuff at: patreon.com/spqr_aeternum")
@@ -43,6 +49,7 @@ print("=========================================================================
 
 
 RECORD_FILE_PATH = "files/tts_read.wav"
+ELEVENLABS_API = "https://api.elevenlabs.io/"
 SCRIPT_OK = True
 
 class SPQRTTSHandler(http.server.BaseHTTPRequestHandler):
@@ -88,6 +95,41 @@ class SPQRTTSHandler(http.server.BaseHTTPRequestHandler):
                 "saved": output_path
             }
             self.wfile.write(json.dumps(response).encode('utf-8'))
+        elif self.path.startswith('/speakelevenlabs'):
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            data = json.loads(post_data.decode('utf-8'))
+            query_params = getParams(self.path)
+            print("Received text to speak ...")
+            
+            headers = {'xi-api-key': self.headers['xi-api-key']}
+            url = ELEVENLABS_API + "v1/text-to-speech/"+query_params["voice"]+"?optimize_streaming_latency="+query_params["optimize_streaming_latency"];
+            try:
+                response = requests.post(url, headers=headers,json=data)
+                response_bytes = response.content
+            except requests.exceptions.RequestException as e:
+                print("Error making ELEVENLABS_API POST request:", e)
+           
+            
+           
+            #Save text
+            output_path = "spqr_tts_latest_elevenlabs.mp3";
+            if query_params['output'] != "":
+                output_path  = query_params['output']
+                
+            absolute_path = os.path.abspath(output_path)
+            with open(absolute_path, 'wb') as f:
+                f.write(response_bytes)
+
+            # Send a response
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            response = {
+                "status": "done",
+                "saved": output_path
+            }
+            self.wfile.write(json.dumps(response).encode('utf-8'))
     def do_GET(self):
         if self.path == '/voices':
             engine = pyttsx3.init()
@@ -101,6 +143,48 @@ class SPQRTTSHandler(http.server.BaseHTTPRequestHandler):
             for voice in voices:
                response[voice.id.split("\\")[-1]] = voice.name
             self.wfile.write(json.dumps(response).encode('utf-8'))
+        elif self.path == '/voiceselevenlabs':
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+                        
+            headers = {'xi-api-key': self.headers['xi-api-key']}
+            url = ELEVENLABS_API + "v1/voices";
+            try:
+                response = requests.get(url, headers=headers)
+                response_string = response.text
+                response_bytes = response_string.encode('utf-8')
+            except requests.exceptions.RequestException as e:
+                print("Error making ELEVENLABS_API GET request:", e)
+            self.wfile.write(response_bytes)
+        elif self.path == '/modelselevenlabs':
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+                        
+            headers = {'xi-api-key': self.headers['xi-api-key']}
+            url = ELEVENLABS_API + "v1/models";
+            try:
+                response = requests.get(url, headers=headers)
+                response_string = response.text
+                response_bytes = response_string.encode('utf-8')
+            except requests.exceptions.RequestException as e:
+                print("Error making ELEVENLABS_API GET request:", e)
+            self.wfile.write(response_bytes)
+        elif self.path == '/userelevenlabs':
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+                        
+            headers = {'xi-api-key': self.headers['xi-api-key']}
+            url = ELEVENLABS_API + "v1/user";
+            try:
+                response = requests.get(url, headers=headers)
+                response_string = response.text
+                response_bytes = response_string.encode('utf-8')
+            except requests.exceptions.RequestException as e:
+                print("Error making ELEVENLABS_API GET request:", e)
+            self.wfile.write(response_bytes)
         elif self.path == '/listen':
             read_text = ""
             read_text_error = ""
@@ -193,7 +277,18 @@ class SPQRTTSHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
 
-
+def getParams(url):
+    query_params = {}
+    if '?' in url:
+        query_string = url.split('?')[1]
+        query_params_list = query_string.split('&')
+        for param in query_params_list:
+            key_value = param.split('=')
+            if len(key_value) == 2:
+                key_value[0] = unquote(key_value[0])
+                key_value[1] = unquote(key_value[1])
+                query_params[key_value[0]] = key_value[1]
+    return query_params
 
 def check_and_install_package(package_name):
     global SCRIPT_OK
@@ -237,6 +332,7 @@ check_and_install_package('openai-whisper')
 
 if SCRIPT_OK:
     import pyttsx3
+    import requests
     import soundfile
     import speech_recognition as sr
     import warnings
